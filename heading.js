be5750ebae3aeb97fae39f7c58ee5c849d20b162
Robbe Anckaert <robbe.anckaert@gmail.com>
2026-05-18 00:32:10 +0200

    Character & Nation Update

--- heading.js
+++ heading.js
@@ -19,11 +19,11 @@ function renderHeader(e) {
             </div>
 
             <div id="header-navigation">
-                <a class="navigation-option center" href="pages/meta/realms/realms.html">Realms</a>
-                <a class="navigation-option center" href="pages/meta/seasons/seasons.html">Seasons</a>
-                <a class="navigation-option center" href="pages/meta/nations/nations.html">Nations</a>
-                <a class="navigation-option center" href="pages/meta/characters/characters.html">Characters</a>
-                <a class="navigation-option center" href="pages/meta/mechanics/mechanics.html">Mechanics</a>
+                <a class="navigation-option center" href="pages/navigation/realms.html">Realms</a>
+                <a class="navigation-option center" href="pages/navigation/seasons.html">Seasons</a>
+                <a class="navigation-option center" href="pages/navigation/nations.html">Nations</a>
+                <a class="navigation-option center" href="pages/navigation/characters.html">Characters</a>
+                <a class="navigation-option center" href="pages/navigation/mechanics.html">Mechanics</a>
             </div>
         `;
     } else {
@@ -34,12 +34,12 @@ function renderHeader(e) {
                 <img src="images/header-logo.webp" alt="Home Button">
             </a>
             <div class="center" id="header-navigation">
-                <a class="navigation-option center" href="pages/meta/realms/realms.html">Realms</a>
-                <a class="navigation-option center" href="pages/meta/seasons/seasons.html">Seasons</a>
-                <a class="navigation-option center" href="pages/meta/nations/nations.html">Nations</a>
-                <a class="navigation-option center" href="pages/meta/characters/characters.html">Characters</a>
-                <a class="navigation-option center" href="pages/meta/players/players.html">Players</a>
-                <a class="navigation-option center" href="pages/meta/mechanics/mechanics.html">Mechanics</a>
+                <a class="navigation-option center" href="pages/navigation/realms.html">Realms</a>
+                <a class="navigation-option center" href="pages/navigation/seasons.html">Seasons</a>
+                <a class="navigation-option center" href="pages/navigation/nations.html">Nations</a>
+                <a class="navigation-option center" href="pages/navigation/characters.html">Characters</a>
+                <a class="navigation-option center" href="pages/navigation/players.html">Players</a>
+                <a class="navigation-option center" href="pages/navigation/mechanics.html">Mechanics</a>
             </div>
             <div id="showcase">${showcaseContent}</div>
         `;
